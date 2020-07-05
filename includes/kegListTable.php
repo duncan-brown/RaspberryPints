3aa77ec410c63033f0a6623e6536457e3d2db6fa
Duncan Brown <dabrown@syr.edu>
2020-07-05 12:16:43 -0400

    add page to show kegs

---
+++ includes/kegListTable.php
@@ -0,0 +1,198 @@
+<?php
+//Only to be called from /includes/common.php printBeerList
+require_once __DIR__.'/functions.php';
+require_once __DIR__.'/../admin/includes/managers/config_manager.php';
+require_once __DIR__.'/../admin/includes/html_helper.php';
+$config = getAllConfigs();
+$htmlHelper = new HtmlHelper();
+$beerColSpan = 1;
+$MAX_COLUMNS = 6;
+$editting = (isset($editingTable) && $editingTable);
+
+$maxTapCol = isset($config[ConfigNames::HozTapListCol])?$config[ConfigNames::HozTapListCol]+1:1;
+if($editting) $maxTapCol = 1;
+?>
+
+<table>
+	<?php if($editting || $config[ConfigNames::ShowBeerTableHead]){?>
+	<?php echo !$editting?'<!-- <thead>':'<tbody>' ?>
+		<tr>
+		<?php for($tapCol = 0; $tapCol< $maxTapCol && $numberOfBeers > $tapCol; $tapCol++){ ?>
+			<?php $beerColSpan = 1; ?>
+			<?php for($col = 1; $col <= $MAX_COLUMNS; $col++){ ?>
+    		
+    			<?php if( beerListShouldDisplayRow($editting, $col, $config[ConfigNames::BeerInfoColNum])){?>
+        			<?php if($config[ConfigNames::ShowBeerName]){ ?>
+        				<?php 
+                            if($config[ConfigNames::ShowBreweryImages]){ $beerColSpan++; }
+                            if($config[ConfigNames::ShowBeerImages]){ $beerColSpan++; }
+                        ?> 
+        				<th <?php if($beerColSpan > 1){ echo 'colspan="'.$beerColSpan.'"';}?> class="beername"  <?php if($maxTapCol!=1) echo 'style="width:'.(100/$maxTapCol).'%";'?>>
+        					<?php if($config[ConfigNames::ShowBeerName]){ ?>
+        						BEER NAME 
+        						<?php if($config[ConfigNames::ShowBeerStyle]){ ?>&nbsp; &nbsp; STYLE<hr><?php } ?>
+        						<?php if($config[ConfigNames::ShowBeerNotes]){ ?>&nbsp; &nbsp; TASTING NOTES<?php } ?>
+        						<?php if($config[ConfigNames::ShowBeerRating]){?>&nbsp; &nbsp; RATING<hr><?php } ?>
+        					<?php } ?>
+							<?php DisplayEditShowColumn($editting, $config, $col, ConfigNames::BeerInfoColNum)?>
+    					<input type="hidden" name="<?php echo ConfigNames::BeerInfoColNum;?>" id="<?php echo ConfigNames::BeerInfoColNum;?>" value="<?php echo abs($config[ConfigNames::BeerInfoColNum]);?>"/>
+        				</th>
+        			<?php } ?>
+    			<?php }?>
+    			
+    			<?php if($config[ConfigNames::ShowKegCol] &&
+				         beerListShouldDisplayRow($editting, $col, $config[ConfigNames::KegColNum])){ ?>
+    				<th class="keg">
+    					DRINKS<hr>REMAINING
+						<?php DisplayEditShowColumn($editting, $config, $col, ConfigNames::KegColNum)?>
+    					<input type="hidden" name="<?php echo ConfigNames::KegColNum;?>" id="<?php echo ConfigNames::KegColNum;?>" value="<?php echo abs($config[ConfigNames::KegColNum]);?>"/>
+    				</th>
+    			<?php } ?>
+			<?php } ?>
+			<?php if($maxTapCol > 1 && $tapCol != $maxTapCol-1){ echo "<td style=width:70px;></td>"; } ?>
+		<?php } ?>
+		</tr>
+	<?php echo !$editting?'</thead>--><tbody>':'' ?>
+	<?php }?>
+		<?php for($i = 1; $i <= ceil($numberOfBeers/$maxTapCol); $i++) {
+			$beer = null;
+			if( isset($beers[$i]) ) $beer = $beers[$i];
+			if($tapOrBottle != ConfigNames::CONTAINER_TYPE_KEG  && !isset($beer) ) continue;
+		?>
+			<tr class="<?php if($i%2 > 0){ echo 'altrow'; }?>" id="<?php echo $beer['id']; ?>">
+				<?php 
+			    for($tapCol = 0; $tapCol< $maxTapCol && $numberOfBeers > $tapCol; $tapCol++){
+        			$beer = null;
+        			if( $i+($tapCol * (ceil($numberOfBeers/$maxTapCol))) > $numberOfBeers ) continue;//Skip numbers outside of the tap range
+        			if( isset($beers[$i+($tapCol * (ceil($numberOfBeers/$maxTapCol)))]) ) $beer = $beers[$i+($tapCol * (ceil($numberOfBeers/$maxTapCol)))];
+        			if($tapOrBottle != ConfigNames::CONTAINER_TYPE_KEG  && !isset($beer) ) continue;
+        		?>
+				<?php for($col = 1; $col <= $MAX_COLUMNS; $col++){ ?>
+			
+			
+				<?php if( beerListShouldDisplayRow($editting, $col, $config[ConfigNames::BeerInfoColNum]) ){?>
+				<?php if($config[ConfigNames::ShowBreweryImages]){ ?>
+					<td class="breweryimg" >
+					<?php if(isset($beer) && $beer['beername']){ ?>
+						<img style="border:0;width:100px" src="<?php echo $beer['breweryImage']; ?>" />
+					<?php } ?>
+					</td>
+				<?php } ?>
+				
+				<?php if($config[ConfigNames::ShowBeerImages]){ ?>
+				<?php /* If not the first column in the beer section 
+				       ($beerColSpan = 1 if just beer 
+				        $beerColSpan = 2 if breweryimg or beerimg and beer, 
+				        $beerColSpan = 3 if all 3
+				 */ ?>
+					<td style="<?php if($beerColSpan > 2){ echo 'border-left: none;'; } ?>" class="beerimg">
+					<?php if(isset($beer) && $beer['beername']){ ?>
+						<?php 
+						beerImg($config, $beer['untID'], $beer['beerId']);
+						?>
+					<?php } ?>
+					</td>
+				<?php } ?>
+                
+				<!-- If not the first column in the beer section-->
+				<td class="name" <?php if($beerColSpan > 1){ echo 'style="border-left: none; width : '.($maxTapCol==1?($beerColSpan > 2?80:90).'%"':'50%"'); } ?>
+							     <?php if($beerColSpan == 1){ echo 'style="width : '.(100/$maxTapCol).'%"'; } ?>>	
+					<?php if(isset($beer) && $beer['beername']){ ?>		
+                    					
+						<?php if($config[ConfigNames::ShowBeerName]){ ?>
+                            <h1><?php echo $beer['beername']; ?></h1>
+                        <?php } ?>
+                        
+					
+					<?php } ?>
+				</td>
+				<?php } ?>
+				
+			<?php if($config[ConfigNames::ShowKegCol] &&
+				         beerListShouldDisplayRow($editting, $col, $config[ConfigNames::KegColNum])){ ?>
+				<td class="keg" >
+				<?php if(isset($beer) && $beer['beername']){ ?>
+				<?php 
+				//Convert to the correct units (use gal and l)
+				    $beer['startAmount']  = convert_volume($beer['startAmount'], $beer['startAmountUnit'], $config[ConfigNames::DisplayUnitVolume], TRUE);
+					$beer['startAmountUnit'] = $config[ConfigNames::DisplayUnitVolume];
+				    $beer['remainAmount'] = convert_volume($beer['remainAmount'], $beer['remainAmountUnit'], $config[ConfigNames::DisplayUnitVolume], TRUE);
+					$beer['remainAmountUnit'] = $config[ConfigNames::DisplayUnitVolume]; 
+				?>
+				<?php } ?>
+				<?php if(isset($beer) && $beer['beername'] && 
+				         $beer['startAmount'] > 0){ ?>
+					<?php if($config[ConfigNames::ShowLastPouredValue] &&
+					         $tapOrBottle == ConfigNames::CONTAINER_TYPE_KEG &&
+					         isset($beer['lastPour']) && $beer['lastPour'] != ''){ ?>
+    					<h3><?php echo $beer['lastPour']?></h3>
+    				<?php }?>
+					<?php if($config[ConfigNames::ShowPouredValue]){?>
+					<?php if($tapOrBottle == ConfigNames::CONTAINER_TYPE_KEG){ ?>
+						<h3><?php echo number_format($beer['startAmount'] - $beer['remainAmount'], 1); echo (is_unit_imperial($config[ConfigNames::DisplayUnitVolume])?"Gal":"L"); ?> poured</h3>
+					<?php } else { ?>
+						<h3><?php echo $beer['remainAmount'].' x '.number_format(convert_volume($beer['volume'], $beer['volumeUnit'], $config[ConfigNames::DisplayUnitVolume]), 1); echo $config[ConfigNames::DisplayUnitVolume];?></h3> 
+					<?php } ?>
+					<?php } ?>
+					<?php 
+    					if($config[ConfigNames::ShowKegImg]){
+    						$kegImgClass = "";
+    						$percentRemaining = 0.0;
+    						if($beer['startAmount'] && $beer['startAmount'] > 0)$percentRemaining = ($beer['remainAmount'] / $beer['startAmount']) * 100;
+    						if( $beer['remainAmount'] <= 0 ) {
+    							$kegImgClass = $tapOrBottle."-empty";
+    							$percentRemaining = 100; 
+    						} else if( $percentRemaining < 15 ) {
+    							$kegImgClass = "-red";
+    						} else if( $percentRemaining < 25 ) {
+    							$kegImgClass = "-orange";
+    						} else if( $percentRemaining < 45 ) {
+    							$kegImgClass = "-yellow";
+    						} else if ( $percentRemaining < 100 ) {
+    							$kegImgClass = "-green";
+    						} else if( $percentRemaining >= 100 ) {
+    							$kegImgClass = "-full";
+    						}
+    						$kegImgClass = strtolower($tapOrBottle).$kegImgClass;
+    						$kegOn = "";
+    						if($config[ConfigNames::UseTapValves]){
+    						    if ( $tapOrBottle == ConfigNames::CONTAINER_TYPE_KEG &&
+    						        $beer['valvePinState'] == $config[ConfigNames::RelayTrigger] ) 
+    								$kegOn = "keg-enabled";
+    							else
+    								$kegOn = "keg-disabled";
+    						}
+					?>
+    					<div class="keg-container">
+    						<?php if($tapOrBottle == ConfigNames::CONTAINER_TYPE_KEG){ ?>
+    							<div class="keg-indicator">
+    								<div class="keg-full <?php echo $kegImgClass ?>" style="height:<?php echo $percentRemaining; ?>%; width: 100%" >
+    								       <div class="<?php echo $kegOn ?>"></div>
+    								</div>
+    							</div>
+    						<?php } else { ?>
+    							<div class="bottle-indicator">
+    								<div class="bottle-full <?php echo $kegImgClass ?>" style="height:<?php echo $percentRemaining; ?>%">
+    								</div>
+    							</div>
+    						<?php } ?>
+    					</div>
+    					<?php }?>
+						<?php if($tapOrBottle == ConfigNames::CONTAINER_TYPE_KEG){ ?>
+							<h3><?php echo number_format($beer['remainAmount'], 1); echo (is_unit_imperial($config[ConfigNames::DisplayUnitVolume])?"Gal":"L"); ?> left</h3>
+						<?php } ?>
+				<?php }elseif( isset($beer) && $beer['beername'] && 
+				               isset($beer['lastPour']) && $beer['lastPour'] != ''){ ?>
+					<?php if($config[ConfigNames::ShowPouredValue]){?>
+						<h3>Last pour:<br/><?php echo $beer['lastPour']?></h3>
+					<?php } ?>
+				<?php }?>
+				</td>
+			<?php } ?>
+			<?php } //End for column loop ?>
+			<?php if($maxTapCol > 1 && $tapCol != $maxTapCol-1){ echo "<td style=width:70px;></td>"; } ?>
+			<?php } //End for tap column loop ?>
+			</tr>
+		<?php } ?>
+	</tbody>
+</table>
---
+++ keg.php
@@ -0,0 +1,225 @@
+<?php
+	if (!file_exists(__DIR__.'/includes/config.php')) {
+		header('Location: install/index.php', true, 303);
+		die();
+	}
+?>
+<?php
+	require_once __DIR__.'/admin/includes/managers/config_manager.php';
+	require_once __DIR__.'/includes/config.php';
+	require_once __DIR__.'/includes/common.php';
+	require_once __DIR__.'/includes/keg.php';
+
+	require_once __DIR__.'/admin/includes/managers/tap_manager.php';
+	require_once __DIR__.'/admin/includes/managers/tempProbe_manager.php';
+	require_once __DIR__.'/admin/includes/managers/bottle_manager.php';
+	require_once __DIR__.'/admin/includes/managers/pour_manager.php'; 
+		
+	$plaatoPins = array(
+	    "style" => 'v64',
+	    "abv" => 'v68',
+	    "og" => 'v65',
+	    "fg" => 'v66',
+	    "remainAmount" => 'v51',
+	    "lastPour" => 'v47',
+	    "temp" => 'v69'
+	);
+	$plaatoTemps = array();
+	//This can be used to choose between CSV or MYSQL DB
+	$db = true;
+	
+	// Setup array for all the beers that will be contained in the list
+	$taps = array();
+	$bottles = array();
+	
+	if($db){
+		// Connect to the database
+		$mysqli = db();		
+		$config = getAllConfigs();
+		
+		$sql =  "SELECT * FROM vwGetActiveTaps";
+		$qry = $mysqli->query($sql);
+		while($b = mysqli_fetch_array($qry))
+		{
+			$beeritem = array(
+				"id" => $b['id'],
+				"beerId" => $b['beerId'],
+				"beername" => $b['name'],
+				"untID" => $b['untID'],
+				"style" => $b['style'],
+		        "brewery" => $b['breweryName'],
+		        "breweryImage" => $b['breweryImageUrl'],
+				"notes" => $b['notes'],
+				"abv" => $b['abv'],
+				"og" => $b['og'],
+				"ogUnit" => $b['ogUnit'],
+				"fg" => $b['fg'],
+				"fgUnit" => $b['fgUnit'],
+				"srm" => $b['srm'],
+				"ibu" => $b['ibu'],
+			    "startAmount" => $b['startAmount'],
+			    "startAmountUnit" => $b['startAmountUnit'],
+			    "remainAmount" => $b['remainAmount'],
+				"remainAmountUnit" => $b['remainAmountUnit'],
+				"tapRgba" => $b['tapRgba'],
+				"tapNumber" => $b['tapNumber'],
+				"rating" => $b['rating'],
+				"srmRgb" => $b['srmRgb'],
+				"valvePinState" => $b['valvePinState'],
+				"plaatoAuthToken" => $b['plaatoAuthToken']
+			);
+			if($config[ConfigNames::UsePlaato]) {
+    			if(isset($b['plaatoAuthToken']) && $b['plaatoAuthToken'] !== NULL && $b['plaatoAuthToken'] != '')
+    			{
+    			    foreach( $plaatoPins as $value => $pin)
+    			    {
+    			        $plaatoValue = file_get_contents("http://plaato.blynk.cc/".$b['plaatoAuthToken']."/get/".$pin);
+    			        $plaatoValue = substr($plaatoValue, 2, strlen($plaatoValue)-4);
+    			        if( $value == 'fg' || $value == 'og' ) $plaatoValue = $plaatoValue/1000;
+    			        if( $value == "temp"){
+    			            if($config[ConfigNames::UsePlaatoTemp])
+    			            {
+    			                $tempInfo["tempUnit"] = (strpos($plaatoValue,"C")?UnitsOfMeasure::TemperatureCelsius:UnitsOfMeasure::TemperatureFahrenheight);
+    			                $tempInfo["temp"] = substr($plaatoValue, 0, strpos($plaatoValue, '°'));
+    			                $tempInfo["probe"] = $b['id'];
+    			                $tempInfo["takenDate"] = date('Y-m-d H:i:s');
+    			                array_push($plaatoTemps, $tempInfo);
+    			            }
+    			            //echo $value."=http://plaato.blynk.cc/".$b['plaatoAuthToken']."/get/".$pin."-".$plaatoTemp.'-'.$plaatoValue.'<br/>';
+        			    }else{
+        			        if( $plaatoValue !== NULL && $plaatoValue != '') $beeritem[$value] = $plaatoValue;
+    			            //echo $value."=http://plaato.blynk.cc/".$b['plaatoAuthToken']."/get/".$pin."-".$beeritem[$value].'-'.$plaatoValue.'<br/>';
+        			    }
+    			        
+    			    }
+    			}
+			}
+			$taps[$b['id']] = $beeritem;
+		}
+		
+		
+		$tapManager = new TapManager();
+		$numberOfTaps = $tapManager->getNumberOfTaps();
+
+		$sql =  "SELECT * FROM vwGetFilledBottles";
+    	$rowNumber = 1;
+		$qry = $mysqli->query($sql);
+		while($b = mysqli_fetch_array($qry))
+		{
+			$beeritem = array(
+				"id" => $b['id'],
+				"beerId" => $b['beerId'],
+				"beername" => $b['name'],
+				"untID" => $b['untID'],
+				"style" => $b['style'],
+		        "brewery" => $b['breweryName'],
+		        "breweryImage" => $b['breweryImageUrl'],
+				"notes" => $b['notes'],
+			    "abv" => $b['abv'],
+			    "og" => $b['og'],
+			    "ogUnit" => $b['ogUnit'],
+			    "fg" => $b['fg'],
+			    "fgUnit" => $b['fgUnit'],
+				"srm" => $b['srm'],
+				"ibu" => $b['ibu'],
+			    "volume" => $b['volume'],
+			    "volumeUnit" => $b['volumeUnit'],
+				"startAmount" => $b['startAmount'],
+				"amountPoured" => $b['amountPoured'],
+				"remainAmount" => $b['remainAmount'],
+				"capRgba" => $b['capRgba'],
+				"capNumber" => $b['capNumber'],
+				"rating" => $b['rating'],
+				"srmRgb" => $b['srmRgb'],
+				"valvePinState" => $b['valvePinState']
+			);
+			$bottles[$rowNumber] = $beeritem;
+      		$rowNumber = $rowNumber+1;
+		}
+		$bottleManager = new BottleManager();
+    	//$bottleManager->UpdateCounts();
+		$numberOfBottles = $bottleManager->getCount();
+		
+		$numberOfPours = 0;
+		if($config[ConfigNames::ShowPourListOnHome]){
+    		$poursManager = new PourManager();
+    		$page = 1;
+    		$limit = $config[ConfigNames::NumberOfDisplayPours];
+    		$totalRows = 0;
+    		$poursList = $poursManager->getLastPours($page, $limit, $totalRows);
+    		$numberOfPours = count($poursList);
+		}
+	}
+		
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>RaspberryPints</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<!-- Set location of Cascading Style Sheet -->
+		<link rel="stylesheet" type="text/css" href="style.css">
+		
+		<?php if($config[ConfigNames::UseHighResolution]) { ?>
+			<link rel="stylesheet" type="text/css" href="style-high-res.css">
+		<?php } ?>
+		
+		<?php	
+		if(! empty($_SERVER['HTTP_USER_AGENT'])){
+    		$useragent = $_SERVER['HTTP_USER_AGENT'];
+    		if( preg_match('@(Android)@', $useragent) ){ ?>
+			<link rel="stylesheet" type="text/css" href="style-aftv.css">
+    	<?php	    
+    		}
+		} ?>
+		
+		<link rel="shortcut icon" href="img/pint.ico">
+<!-- <meta name="viewport" content="initial-scale=0.7,width=device-width,height=device-height,target-densitydpi=device-dpi,user-scalable=yes" />  -->		
+		<script type="text/javascript" src="admin/scripts/ws.js"></script>	
+    <style>
+        body { background-image: url('img/beer-mugs.jpg'); }
+        table { width  : 99%; }
+        table h1 { font-size: 6em; color: rgb(0,0,200); -webkit-text-stroke-width: 4px; -webkit-text-stroke-color: yellow; }
+        table h3 { color: yellow; }
+    </style>
+
+	</head> 
+
+<!--<body> -->
+<body onload="wsconnect(); <?php if($config[ConfigNames::ShowTempOnMainPage])echo "setTimeout(function(){window.location.reload(1);}, 60000);"; ?>">
+		<div class="bodywrapper" id="mainTable">
+			
+			<?php 
+				if($numberOfTaps > 0)printKegList($taps, $numberOfTaps, ConfigNames::CONTAINER_TYPE_KEG);
+			?>
+		</div>
+		<!-- <div class="copyright">Data provided by <a href="http://untappd.com">Untappd</a></div> -->
+		
+		<?php if($config[ConfigNames::DisplayRowsSameHeight]) { ?>
+		<script type="text/javascript">
+		window.onload = function(){
+			tables = document.getElementsByTagName("table")
+			for (var i = 0; i < tables.length; i++) {
+			    var table = tables[i];		
+				maxHeight = -1;
+				//Start at 1 to avoid header row
+				for (var j = 1; j < table.rows.length; j++) {
+				    var row = table.rows[j];		
+					if( row.offsetHeight > maxHeight ) maxHeight = row.offsetHeight;
+				}
+				if( maxHeight > 0 ){
+    				for (var j = 1; j < table.rows.length; j++) {
+    				    var row = table.rows[j];	
+    					row.style.height = maxHeight + 'px';
+    				}
+				}
+			}
+
+			wsconnect();
+
+			<?php if($config[ConfigNames::ShowTempOnMainPage])echo "setTimeout(function(){window.location.reload(1);}, 60000);"; ?>
+		}
+		</script>
+		<?php } ?>
+	</body>
+</html>
